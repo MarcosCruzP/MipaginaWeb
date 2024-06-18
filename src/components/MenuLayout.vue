89644e6d1212b4129674ef685b5b27c184390992
Marcos <marcos.cpascacio@gmail.com>
2024-06-18 01:44:52 -0600

    Mi web 1

---
+++ src/components/MenuLayout.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-layout>
+    <v-app-bar density="compact" :elevation="0" color="#f2eeea">
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer">
+        <v-icon icon="mdi-apps"></v-icon>
+      </v-app-bar-nav-icon>
+
+      <!-- <v-toolbar-title>My files</v-toolbar-title> -->
+      <v-spacer></v-spacer>
+      Marcos Cruz Pascacio
+      <v-spacer></v-spacer>
+      <!-- <v-avatar>
+             
+            </v-avatar> -->
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" :rail="false" permanent>
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-menu location="end" transition="slide-x-transition">
+          <template v-slot:activator="{ props: menu }">
+            <v-tooltip location="end">
+              <template v-slot:activator="{ props: tooltip }">
+                <v-list-item
+                  v-bind="mergeProps(menu, tooltip)"
+                  prepend-icon="mdi-account"
+                  title="My Account"
+                  value="account"
+                ></v-list-item>
+              </template>
+              <span>I'm A Tooltip</span>
+            </v-tooltip>
+          </template>
+          <v-list style="margin-left: 10px">
+            <RouterLink :to="{ name: 'users' }">
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </RouterLink>
+          </v-list>
+        </v-menu>
+      </v-list>
+
+      <template v-slot:append>
+        <v-list>
+          <v-list-item prepend-icon="mdi-logout" title="My Account"></v-list-item>
+          <v-list-item prepend-icon="mdi-cog" title="My Account"></v-list-item>
+        </v-list>
+      </template>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { mergeProps } from 'vue'
+import { RouterLink } from 'vue-router'
+
+var drawer = ref(false)
+var items = ref([
+  { title: 'Click Me1' },
+  { title: 'Click Me2' },
+  { title: 'Click Me3' },
+  { title: 'Click Me4' }
+])
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- <v-row style="">
+    <div style="" class="line"></div>
+    dfsdf</v-row
+  > -->
+  <div style="display: flex; height: 94vh; width: 100%; color: #0000; background-color: #f2eeea">
+    <div style="" class="line"></div>
+    <div style="" class="line2"></div>
+    <div class="presentacion">
+      <div class="name"><b>Marcos Cruz Pascacio</b></div>
+      <div class="rol">Desarrollador IoT</div>
+      <div class="detailp">
+        Dogitalizar, automatizar y optimizar procesos mediante la digitalizacion e IOT es lo que
+        hago.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playwrite+IS:wght@100..400&display=swap');
+.line {
+  width: 1px;
+  height: 100%;
+
+  background-color: black;
+}
+.line2 {
+  width: 3px;
+  margin-top: 30vh;
+  height: 10vh;
+  background-color: black;
+}
+
+.presentacion {
+  margin-right: 10%;
+  color: black;
+  font-size: medium;
+  width: 30%;
+  height: 40vh;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30vh;
+  margin-left: 5%;
+}
+.name {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 42px;
+}
+.rol {
+  margin-top: 10px;
+  font-family: 'Playwrite IS', cursive;
+  font-size: medium;
+}
+
+.detailp {
+  margin-top: 15px;
+  width: 55%;
+  font-size: 19px;
+}
+</style>
